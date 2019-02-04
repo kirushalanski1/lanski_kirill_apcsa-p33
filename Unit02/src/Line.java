309ecc19c104af1bdb5ea931a6282b1a72730071
kirushalanski1 <47193575+kirushalanski1@users.noreply.github.com>
2019-02-04 09:26:59 -0800

    Completed Unit 02.

---
+++ Unit02/src/Line.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Line
+{
+	public static double getSlope( int x1, int y1, int x2, int y2 )
+	{
+		return (y2 - (double)y1)/(x2 - x1);
+	}
+
+}
