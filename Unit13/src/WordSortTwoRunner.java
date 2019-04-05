7568e1ad74630b366fb9129bf570ba55636421b7
kirushalanski1 <47193575+kirushalanski1@users.noreply.github.com>
2019-04-05 12:02:25 -0700

    Finished Unit 13.

---
+++ Unit13/src/WordSortTwoRunner.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import static java.lang.System.*;
+
+public class WordSortTwoRunner
+{
+	public static void main( String args[] ) throws IOException
+	{
+		Scanner file = new Scanner(new File("C:\\Users\\kirus\\OneDrive\\Documents\\AP CSA\\Unit13Students\\Unit13-Assignments\\wordsorttwo.dat"));
+		int size = file.nextInt();
+		file.nextLine();
+		for(int i = 0; i<size; i++)
+		{
+			String sentence = file.nextLine();
+			WordSortTwo test = new WordSortTwo(sentence);
+			test.sort();
+			System.out.println(test);
+		}
+	}
+}
